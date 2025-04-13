63377eecfd55e89ad0608cc0a5a57cdcd1085530
Henrique Oliveira <hrq1408@gmail.com>
2025-04-13 18:27:56 -0300

    React 18 + Vite + TypeScript + Nginx + MariaDB + Docker

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-6 text-xl text-gray-800">
+      <h1>📰 Sistema de Notícias</h1>
+      <p>Frontend em React + TypeScript rodando com Vite!</p>
+    </div>
+  )
+}
+
+export default App;
