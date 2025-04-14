23bc1feeb14d8e71f2c52e13745036d4534f0865
Henrique Oliveira <hrq1408@gmail.com>
2025-04-13 23:09:09 -0300

    Block minor adjustments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,50 @@ import { NoticiaResumo } from './types/NoticiaResumo'
 
 const noticiasMock: NoticiaResumo[] = [
   {
-    id: 1,
+    "id": 1,
+    "editoria": "Economia",
+    "url": "https://www.estadao.com.br/economia/china-nova-retaliacao-tarifas-estados-unidos/",
+    "titulo": "China retalia os EUA, eleva tarifas para 125%, mas diz que disputa se tornará ‘piada’ histórica",
+    "subtitulo": "China diz que guerra se tornou ‘um jogo de números, que não tem nenhum significado econômico prático’, e sinalizou que não fará novos aumentos, mesmo que os Estados Unidos voltem a subir as taxas",
+    "data_hora_publicacao": "2025-04-11 16:44:00",
+    "imagem": "https://www.estadao.com.br/resizer/v2/LXVZ7DQIOZEEHDX5NOAYSR5PCA.jpg?quality=80&auth=f3286120503bf42b9c65bf076baa9ff9448f1ddf9864109ca5d59b4276df205d&width=1200",
+    "imagem_thumb": "https://www.estadao.com.br/resizer/v2/LXVZ7DQIOZEEHDX5NOAYSR5PCA.jpg?quality=80&auth=f3286120503bf42b9c65bf076baa9ff9448f1ddf9864109ca5d59b4276df205d&width=380",
+    "conteudo": "<p>A <a href=\"https://www.estadao.com.br/tudo-sobre/china-asia/?srsltid=AfmBOor1Cfrei_s80QNDtk3KPwvs5lJPBgFSUq3QA8VwCMFZigH7FNs8\" target=\"_blank\"><b>China</b></a> anunciou nesta sexta-feira que aumentará as tarifas sobre os produtos norte-americanos de 84% para 125% a partir deste sábado, 12. Washington e Pequim vêm subindo o tom de sua guerra comercial ao aumentarem as tarifas entre si, mesmo depois que o presidente dos EUA, <a href=\"https://www.estadao.com.br/tudo-sobre/donald-trump/?srsltid=AfmBOopkVngEZd_fLs30gyjl2Xcsm6KEm7amcIm5RaMOL6zqli3BLy7n\" target=\"_blank\"><b>Donald Trump</b></a>, suspendeu temporariamente as cobranças retaliatórias sobre outros países.</p><p> A China havia dito que combateria as tarifas dos EUA com suas próprias contramedidas e chamou as ações de Trump de “assédio econômico”, levando o líder republicano a retaliar aumentando substancialmente as tarifas nesta semana.</p><p>As <a href=\"https://www.estadao.com.br/economia/tarifa-china-reciprocas-eua/?srsltid=AfmBOopFjYGG6OxlTumxR1y9QoZc4j5DJIiAz1milV9yqlfGzRxeJ5ml\" target=\"_blank\"><b>tarifas totais de Trump sobre Pequim são de 145%</b></a>. Ontem, ele disse que, quando ele anunciou na quarta-feira, 9,  que o gigante asiático enfrentaria tarifas de 125%, não havia incluído as tarifas de 20% anunciadas em fevereiro e março.</p><p><div>                            <figure><img                                    loading=\"lazy\"                                    class=\"size-full\"                                    src=\"https://www.estadao.com.br/resizer/v2/LXVZ7DQIOZEEHDX5NOAYSR5PCA.jpg?quality=80&auth=f3286120503bf42b9c65bf076baa9ff9448f1ddf9864109ca5d59b4276df205d&width=380\"                                    alt=\"Saul Loeb/AFP\" />                                <figcaption>Os presidentes dos Estados Unidos, Donald Trump (E) e da China, Xi Jinping Foto: Saul Loeb/AFP</figcaption>                            </figure>                        </div></p><p>A China sinalizou também que não fará novos aumentos, mesmo que os EUA voltem a subir as tarifas.  “Dado que, com este nível de tarifas, os produtos dos EUA exportados para a China não têm mais chance de serem aceitos no mercado”, se Washington continuar a aumentar suas tarifas, “a China vai ignorar isso”, disse a Comissão Tarifária do governo em um comunicado divulgado pelo Ministério das Finanças.</p><p>As medidas de Trump levaram economistas e empresários a alertar sobre uma possível recessão, e alguns dos principais parceiros comerciais de Washington a retaliar com seus próprios impostos de importação, antes da pausa na aplicação. No entanto, Trump e a China continuaram a aumentar as tarifas em um processo mútuo.</p><p>“O aumento anormalmente alto das tarifas dos Estados Unidos sobre a China tornou-se um jogo de números, que não tem nenhum significado econômico prático, e se tornará uma piada na história da economia mundial”, disse um porta-voz do Ministério das Finanças chinês ao anunciar as novas tarifas. “Entretanto, se os Estados Unidos insistirem em continuar a infringir substancialmente os interesses da China, a China revidará com determinação e lutará até o fim”.</p><p>O Ministério do Comércio da China disse que apresentaria outra queixa à <a href=\"https://www.estadao.com.br/tudo-sobre/omc-organizacao-mundial-do-comercio/?srsltid=AfmBOopSxnDfCwGLxHIMxPz48imk2a3uxv4V9RFwd7s6bqe2xdvw4XQD\" target=\"_blank\"><b>Organização Mundial do Comércio</b></a> contra as tarifas de Washington.<div class=\"lista-numerada-noticia list-numbered modulo-noticia\"><h3 class=\"lista-numerada-title numbered-title\"></h3><ul class=\"indice-ranking\"><li><a href=\"https://www.estadao.com.br/economia/the-economist-politica-comercial-trump-danos-duradouros/\" title=\"The Economist: A política comercial incoerente de Trump causará danos duradouros à economia mundial\"><figure class=\"image-ranking\"><div class=\"pos \">1</div></figure><div class=\"ln-right\"><div class=\"ln-right-title title-ranking\">The Economist: A política comercial incoerente de Trump causará danos duradouros à economia mundial</div><p class=\"description-ranking\"></div></a></li><li><a href=\"https://www.estadao.com.br/economia/politica-tarifaria-trump-efeito-colateral-entrevista/\" title=\"‘Política de Trump é meio desastrada, mas talvez tenha efeito colateral positivo’, diz Samuel Pessôa\"><figure class=\"image-ranking\"><div class=\"pos \">2</div></figure><div class=\"ln-right\"><div class=\"ln-right-title title-ranking\">‘Política de Trump é meio desastrada, mas talvez tenha efeito colateral positivo’, diz Samuel Pessôa</div><p class=\"description-ranking\"></div></a></li><li><a href=\"https://www.estadao.com.br/economia/por-que-venda-em-massa-titulos-eua-fez-trump-recuar-tarifas/\" title=\"Entenda por que a venda em massa de títulos dos EUA fez Trump recuar nas tarifas\"><figure class=\"image-ranking\"><div class=\"pos \">3</div></figure><div class=\"ln-right\"><div class=\"ln-right-title title-ranking\">Entenda por que a venda em massa de títulos dos EUA fez Trump recuar nas tarifas</div><p class=\"description-ranking\"></div></a></li></ul></div></p><p>Na semana passada, Pequim suspendeu as importações de sorgo, aves e farinha de ossos de algumas empresas norte-americanas e aumentou os controles de exportação de minerais de terras raras essenciais para várias tecnologias, além de colocar algumas dezenas de empresas norte-americanas em listas que impediriam as empresas chinesas de vender-lhes produtos de uso duplo.</p><p>Considerando o tamanho das duas economias, as maiores do mundo, os especialistas temem um caos econômico global.</p><p>O diretor-geral da OMC, Ngozi Okonjo-Iweala, disse no início desta semana que uma guerra comercial entre Washington e Pequim poderia “prejudicar gravemente as perspectivas econômicas globais”.</p>"
+ },
+  {
+    id: 2,
     titulo: "Reportagens do 'Estadão' sobre efeitos da pandemia em crianças são premiadas",
     url: 'https://www.estadao.com.br',
-    imagem: 'https://via.placeholder.com/400x200',
+    imagem: 'https://dummyjson.com/image/400x200',
     dataHoraPublicacao: '2025-04-13T12:00:00Z',
   },
-  // outras notícias
+  {
+    id: 3,
+    titulo: "Reportagens do 'Estadão' sobre efeitos da pandemia em crianças são premiadas",
+    url: 'https://www.estadao.com.br',
+    imagem: 'https://dummyjson.com/image/400x200',
+    dataHoraPublicacao: '2025-04-13T12:00:00Z',
+  },
+  {
+    id: 4,
+    titulo: "Reportagens do 'Estadão' sobre efeitos da pandemia em crianças são premiadas",
+    url: 'https://www.estadao.com.br',
+    imagem: 'https://dummyjson.com/image/400x200',
+    dataHoraPublicacao: '2025-04-13T12:00:00Z',
+  },
+  {
+    id: 5,
+    titulo: "Reportagens do 'Estadão' sobre efeitos da pandemia em crianças são premiadas",
+    url: 'https://www.estadao.com.br',
+    imagem: 'https://dummyjson.com/image/400x200',
+    dataHoraPublicacao: '2025-04-13T12:00:00Z',
+  },
 ]
 
 function App() {
   return (
-    <div className="p-6 text-xl text-gray-800">
-      <h1>📰 Sistema de Notícias</h1>
-      <p>Frontend em React + TypeScript rodando com Vite!</p>
+    <div className="container-fluid text-center p-6 text-xl text-gray-800">
+      <h1 className="mb-8 mt-5">📰 Estadão - News Manager  📰</h1>
       <NoticiaLista noticias={noticiasMock} />
     </div>
   )
--- frontend/src/components/NoticiaCard.tsx
+++ frontend/src/components/NoticiaCard.tsx
@@ -1,10 +1,12 @@
 // components/NoticiaCard.tsx
 import React from 'react'
+import { Bookmark, MessageSquare, Share } from 'lucide-react'
 
 interface NoticiaCardProps {
   noticia: {
     id: number
-    titulo: string
+    titulo: string,
+    editoria: string,
     url: string
     imagem: string
     dataHoraPublicacao: string
@@ -15,23 +17,43 @@ export const NoticiaCard: React.FC<NoticiaCardProps> = ({ noticia }) => {
   return (
     <a
       href={noticia.url}
-      className="block rounded overflow-hidden border border-gray-300 bg-white hover:shadow-lg transition duration-300"
+      className="block rounded-[.7rem] overflow-hidden border border-gray-300 bg-white hover:shadow-lg transition duration-300"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="flex flex-col h-full">
+      <div className="flex flex-col h-full text-left">
         <img
           src={noticia.imagem}
           alt={noticia.titulo}
-          className="w-full h-52 object-cover"
+          className="w-full h-[14rem] object-cover"
         />
-        <div className="p-4 flex flex-col justify-between flex-1">
-          <h2 className="text-[17px] font-semibold text-[#0d3b66] leading-tight mb-2 line-clamp-2">
+
+        <div className="p-4 pt-3 flex flex-col justify-between flex-1">
+          <span className="text-[.75rem] text-blue-700 font-semibold uppercase">
+            {noticia.editoria}
+          </span>
+
+          <h2 className="text-[1.05rem] font-bold text-[#0d3b66] leading-snug mb-1 line-clamp-2">
             {noticia.titulo}
           </h2>
-          <p className="text-xs text-gray-500">
-            {new Date(noticia.dataHoraPublicacao).toLocaleDateString('pt-BR')}
+
+          <p className="text-[.85rem] text-gray-500 mb-4">
+          {noticia.titulo}
           </p>
+
+          <hr className='mb-3'/>
+
+          <div className="flex justify-end items-center text-gray-400">
+            {/* <span className="text-xs">
+              {new Date(noticia.dataHoraPublicacao).toLocaleDateString('pt-BR')}
+            </span> */}
+
+            <div className="flex gap-3 text-gray-400">
+                <Share className="w-5 h-6 hover:text-blue-600 transition" />
+                <MessageSquare className="w-5 h-6 hover:text-blue-600 transition" />
+                <Bookmark className="w-5 h-6 hover:text-blue-600 transition" />
+            </div>
+          </div>
         </div>
       </div>
     </a>
