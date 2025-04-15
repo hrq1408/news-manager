2110b951cb99ebdb52573a45cabd406a20a17ca9
Henrique Oliveira <hrq1408@gmail.com>
2025-04-15 19:41:38 -0300

    Added Prisma Seeds and Env Example

---
+++ backend/src/scripts/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client'
+import noticias from '../../noticias'
+const prisma = new PrismaClient()
+
+async function main() {
+  for (const noticia of noticias) {
+    await prisma.noticia.upsert({
+      where: { id: noticia.id },
+      update: {},
+      create: {
+        id: noticia.id,
+        titulo: noticia.titulo,
+        subtitulo: noticia.subtitulo,
+        url: noticia.url,
+        imagem: noticia.imagem,
+        editoria: noticia.editoria,
+        data_hora_publicacao: new Date(noticia.data_hora_publicacao),
+        conteudo: noticia.conteudo,
+      },
+    })
+  }
+  console.log('Database populed')
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(() => {
+    prisma.$disconnect()
+  })
