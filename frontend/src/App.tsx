787b88cf89757873b665b8112433b43154072187
Henrique Oliveira <hrq1408@gmail.com>
2025-04-16 04:13:32 -0300

    new path & axios installation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { NoticiaLista } from './pages/NoticiaLista'
 import { Routes, Route } from 'react-router-dom'
 import { NoticiaCompleta } from './pages/NoticiaCompleta'
+import { Admin } from './pages/Admin';
 
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/admin" element={<Admin/>} />
       <Route path="/" element={<NoticiaLista/>} />
       <Route path="/noticia/:id" element={<NoticiaCompleta />} />
     </Routes>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,29 @@ module.exports = {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {}
     },
     plugins: [],
   }
   
+  
+  // trocar as cores
+
+  // black blue
+  // rgb(5 12 22)
+
+  // blue
+  // rgb(48 123 230)
+
+  // light gray
+  // rgb(97 119 144)
+
+  // segunda página com o id da matéria (react-router)
+  // pagina de admin com os CRUDs
+  // criar controle de estado para o admin
+
+  // criar o banco de dados para trazer as matérias
+
+  // testes unitários dos módlos principais
+
+  // garantir o MVC no backend
