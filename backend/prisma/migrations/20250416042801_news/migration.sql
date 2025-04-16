e7fbcc22eacd355d24b195898df3ad45a2ab00c1
Henrique Oliveira <hrq1408@gmail.com>
2025-04-16 02:57:50 -0300

    Prisma generate new schema - fix type subtitulo

--- backend/prisma/migrations/20250416042801_news/migration.sql
+++ backend/prisma/migrations/20250416042801_news/migration.sql
@@ -2,12 +2,12 @@
 CREATE TABLE `Noticia` (
     `id` INTEGER NOT NULL,
     `titulo` VARCHAR(191) NOT NULL,
-    `subtitulo` VARCHAR(191) NOT NULL,
+    `subtitulo` TEXT NOT NULL,
     `url` VARCHAR(191) NOT NULL,
     `imagem` VARCHAR(191) NOT NULL,
     `editoria` VARCHAR(191) NOT NULL,
     `data_hora_publicacao` DATETIME(3) NOT NULL,
-    `conteudo` VARCHAR(191) NOT NULL,
+    `conteudo` TEXT NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- backend/src/scripts/seed.ts
+++ backend/src/scripts/seed.ts
@@ -19,7 +19,7 @@ async function main() {
       },
     })
   }
-  console.log('Database populed')
+  console.log('Database populated')
 }
 
 main()
